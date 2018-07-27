cdf7f1d9012978dcc4fa5f01915aa71f215ad6f3
Renandika Galih <rengalih666@gmail.com>
2018-07-27 23:18:24 +0700

    Revert "Merge branch 'modul-login'"
    This reverts commit 5f47b3a5325bcf967282cdef055bdf868469fac1, reversing
    changes made to af488761c476db58a7c9cb434a1bc25d9e4398c1.

--- application/views/templates/admin_tpl/admin_tpl.php
+++ application/views/templates/admin_tpl/admin_tpl.php
@@ -41,11 +41,6 @@
 		}
 		echo "\n\t";
 		?>
-
-		<!-- jQuery -->
-		<script src="<?php echo base_url().'assets/admin_assets/'; ?>vendors/jquery/dist/jquery.min.js"></script>
-		<!-- Bootstrap -->
-		<script src="<?php echo base_url().'assets/admin_assets/'; ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 	</head>
 	<body class="nav-md footer_fixed">
 		<?php echo $this->load->get_section('sidebar_menu'); ?>
@@ -59,6 +54,11 @@
 		<!-- /page content -->
 
 		<?php echo $this->load->get_section('footer'); ?>
+
+		<!-- jQuery -->
+		<script src="<?php echo base_url().'assets/admin_assets/'; ?>vendors/jquery/dist/jquery.min.js"></script>
+		<!-- Bootstrap -->
+		<script src="<?php echo base_url().'assets/admin_assets/'; ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 		<!-- FastClick -->
 		<script src="<?php echo base_url().'assets/admin_assets/'; ?>vendors/fastclick/lib/fastclick.js"></script>
 		<!-- NProgress -->
