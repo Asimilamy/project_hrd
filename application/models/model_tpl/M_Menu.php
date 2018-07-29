c5c99f07cda32eac2023e3cf50bd2a9d7436f924
Renandika Galih <rengalih666@gmail.com>
2018-07-29 13:27:05 +0700

    first-commit

---
+++ application/models/model_tpl/M_Menu.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed!');
+
+class M_Menu extends CI_Model {
+	public function get_user_menu($kd_user = '') {
+
+	}
+}
--- application/views/templates/admin_tpl/sidebar_menu.php
+++ application/views/templates/admin_tpl/sidebar_menu.php
@@ -26,6 +26,16 @@
 				<!-- sidebar menu -->
 				<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 					<div class="menu_section">
+						<h3>Test Menu</h3>
+						<ul class="nav side-menu">
+							<li>
+								<a href="<?php echo base_url('administrator/home'); ?>">
+									<i class="fa fa-dashboard"></i> Home
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="menu_section">
 						<h3>General</h3>
 						<ul class="nav side-menu">
 							<li>
