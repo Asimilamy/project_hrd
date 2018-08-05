4f7433f09b094897e6543f4fac94cf115b248bfe
asimilamy <rengalih666@gmail.com>
2018-08-05 21:51:43 +0700

    update-htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = substr($_SERVER['PHP_SELF'], 0, -9);
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
