8cdcea695309c7be9e1d4419b1dec0dbb6ff9b51
Renandika Galih <rengalih666@gmail.com>
2018-11-22 16:20:22 +0700

    updates-master-asuransi

--- application/models/model_asuransi/Tm_asuransi.php
+++ application/models/model_asuransi/Tm_asuransi.php
@@ -9,7 +9,7 @@ class Tm_asuransi extends CI_Model {
 	public function ssp_table() {
 		$data['table'] = $this->tbl_name;
 
-		$data['primaryKey'] = 'a.'.$this->p_key;
+		$data['primaryKey'] = $this->p_key;
 
 		$data['columns'] = array(
 			array( 'db' => $this->p_key,
@@ -18,21 +18,13 @@ class Tm_asuransi extends CI_Model {
 
 					return $this->tbl_btn($d, $row[2]);
 				} ),
-			array( 'db' => 'a.nik_karyawan', 'dt' => 2, 'field' => 'nik_karyawan' ),
-			array( 'db' => 'a.nm_karyawan', 'dt' => 3, 'field' => 'nm_karyawan' ),
-			array( 'db' => 'b.nm_unit', 'dt' => 4, 'field' => 'nm_unit' ),
-			array( 'db' => 'c.nm_bagian', 'dt' => 5, 'field' => 'nm_bagian' ),
-			array( 'db' => 'd.nm_jabatan', 'dt' => 6, 'field' => 'nm_jabatan' ),
-			array( 'db' => 'e.nm_status_kerja', 'dt' => 7, 'field' => 'nm_status_kerja' ),
-			array( 'db' => 'a.tgl_aktif', 'dt' => 8, 'field' => 'tgl_aktif',
-				'formatter' => function($d) {
-					return format_date($d, 'd-m-Y');
-				} ),
+			array( 'db' => $this->p_key, 'dt' => 2, 'field' => $this->p_key ),
+			array( 'db' => 'nm_asuransi', 'dt' => 3, 'field' => 'nm_asuransi' ),
 		);
 
 		$data['sql_details'] = sql_connect();
 
-		$data['joinQuery'] = 'FROM '.$this->tbl_name.' a LEFT JOIN tm_unit b ON b.kd_unit = a.unit_kd LEFT JOIN tm_bagian c ON c.kd_bagian = a.bagian_kd LEFT JOIN tm_jabatan d ON d.kd_jabatan = a.jabatan_kd LEFT JOIN tm_status_kerja e ON e.kd_status_kerja = a.status_kerja_kd';
+		$data['joinQuery'] = '';
 
 		$data['where'] = '';
 
@@ -61,28 +53,22 @@ class Tm_asuransi extends CI_Model {
 		$this->load->model(array('model_basic/base_query'));
 		$row = $this->base_query->get_row($this->tbl_name, array($this->p_key => $id));
 		if (!empty($row)) :
-			$data = array('kd_karyawan' => $row->kd_karyawan, 'nik_karyawan' => $row->nik_karyawan, 'nm_karyawan' => $row->nm_karyawan, 'status_kerja_kd' => $row->status_kerja_kd, 'unit_kd' => $row->unit_kd, 'bagian_kd' => $row->bagian_kd, 'jabatan_kd' => $row->jabatan_kd, 'tgl_masuk' => format_date($row->tgl_aktif, 'd-m-Y'));
+			$data = array('kd_asuransi' => $row->kd_asuransi, 'nm_asuransi' => $row->nm_asuransi);
 		else :
-			$data = array('kd_karyawan' => '', 'nik_karyawan' => '', 'nm_karyawan' => '', 'status_kerja_kd' => '', 'unit_kd' => '', 'bagian_kd' => '', 'jabatan_kd' => '', 'tgl_masuk' => '');
+			$data = array('kd_asuransi' => '', 'nm_asuransi' => '');
 		endif;
 		return $data;
 	}
 
 	public function form_rules() {
 		$rules = array(
-			array('field' => 'txtNik', 'label' => 'NIK Karyawan', 'rules' => 'required'),
-			array('field' => 'txtNm', 'label' => 'Nama Karyawan', 'rules' => 'required'),
-			array('field' => 'selStatusKerja', 'label' =>  'Status Kerja', 'rules' => 'required'),
-			array('field' => 'selUnit', 'label' => 'Unit', 'rules' => 'required'),
-			array('field' => 'selBagian', 'label' => 'Bagian', 'rules' => 'required'),
-			array('field' => 'selJabatan', 'label' => 'Jabatan', 'rules' => 'required'),
-			array('field' => 'txtTglMasuk', 'label' => 'Tanggal Masuk', 'rules' => 'required'),
+			array('field' => 'txtNm', 'label' => 'Nama Asuransi', 'rules' => 'required'),
 		);
 		return $rules;
 	}
 
 	public function form_warning($datas = '') {
-		$forms = array('txtNik', 'txtNm', 'selStatusKerja', 'selUnit', 'selBagian', 'selJabatan', 'txtTglMasuk');
+		$forms = array('txtNm');
 		foreach ($datas as $key => $data) :
 			$str[$data] = (!empty(form_error($forms[$key])))?build_label('warning', form_error($forms[$key], '"', '"')):'';
 		endforeach;
