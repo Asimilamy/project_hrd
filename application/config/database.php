cb1cb694e2f47a3aef9cb7457be9ca275a58e3cb
Renandika Galih <rengalih666@gmail.com>
2019-03-27 12:41:08 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost; dbname=db_project_hrd; charset=utf8;',
-	'hostname' => 'localhost',
-	'username' => 'root',
+	'dsn'	=> '',
+	'hostname' => '',
+	'username' => '',
 	'password' => '',
-	'database' => 'db_project_hrd',
-	'dbdriver' => 'pdo',
+	'database' => '',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
